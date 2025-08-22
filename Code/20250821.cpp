d07186784375ffc89d35792a182e22f2d07ba82e
John D <raydeight@users.noreply.github.com>
2025-08-21 23:08:20 -0700

    Cpp

--- Code/20250821.cpp
+++ Code/20250821.cpp
@@ -4,13 +4,26 @@ using namespace std;
 
 int main()
 {
-    int A[10] = { 2,4,6,8,10,12,14 };
+    // int A[10] = { 2,4,6,8,10,12,14 };
     //A[0] = 12;
     //A[1] = 15;
     //A[2] = 25;
-
-    cout << sizeof(A) << endl;
-    cout << A[8] << endl;
-    printf("%d\n", A[9]);
+    int n;
+    cout << "Enter Size";
+    cin >> n;
+    //int A[n] = {2,4,6,8,10,12};// cannot initialize an array like this
+    //variable size array cannot be initialized
+    A[0] = 2;
+    for (int x : A) // for each loop
+    {
+        cout << x << endl;
+    }
+    //for (int i = 0; i < 10; i++)
+    //{
+    //    cout << A[i] << endl;
+    //}
+    // cout << sizeof(A) << endl;
+    // cout << A[8] << endl;
+    // printf("%d\n", A[9]);
     return 0;
 }
