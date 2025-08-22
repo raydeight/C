35ff9a0ad140d2264ba10bbee1ef2489af3cba96
John D <raydeight@users.noreply.github.com>
2025-08-21 22:59:38 -0700

    Cpp

--- Code/20250821.cpp
+++ Code/20250821.cpp
@@ -4,11 +4,13 @@ using namespace std;
 
 int main()
 {
-    int A[5];
-    A[0] = 12;
-    A[1] = 15;
-    A[2] = 25;
+    int A[10] = { 2,4,6,8,10,12,14 };
+    //A[0] = 12;
+    //A[1] = 15;
+    //A[2] = 25;
 
-    cout<<sizeof(A);
+    cout << sizeof(A) << endl;
+    cout << A[8] << endl;
+    printf("%d\n", A[9]);
     return 0;
 }
